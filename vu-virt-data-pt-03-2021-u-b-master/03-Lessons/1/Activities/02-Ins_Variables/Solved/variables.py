4abbc758c782bf079a80659e8b177093ceaf39b9
lmiles77 <80069183+lmiles77@users.noreply.github.com>
2021-06-16 16:29:07 -0500

    add files

---
+++ vu-virt-data-pt-03-2021-u-b-master/03-Lessons/1/Activities/02-Ins_Variables/Solved/variables.py
@@ -0,0 +1,17 @@
+# Creates a variable with a string "Frankfurter"
+title = 'Frankfurter'
+
+# Creates a variable with an integer 80
+years = 60
+
+# Creates a variable with the boolean value of True
+expert_status = True
+
+# Prints a statement adding the variable
+print("Nick is a professional " + title)
+
+# Convert the integer years into a string and prints
+print("He has been coding for " + str("2") + "years")
+
+# An f-string accepts all data types without conversion
+print(f"Expert status: {expert_status}")
